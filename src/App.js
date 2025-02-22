d8f58c541cd48ba04620b01c0f49d68df16d9be0
Sachin2123 <sachinvishwakarma8286@gmail.com>
2025-02-22 18:41:39 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Header from "../src/components/Header/Header";
 import UserDetails from "../src/components/UserDetails/UserDetails";
 import Footer from "../src/components/Footer/footer";
-import Sidebar from "./components/Sidebar/Sidebar";
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Sidebar /> */}
       <UserDetails />
       <Footer />
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,33 +7,21 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
 
 const pages = ["Products", "Pricing", "Blog"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function Header() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
   };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
   return (
     <div className="App">
       <AppBar
